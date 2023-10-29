c5acac025e239f1119509c51fa9289c9b2d6c682
delso-ferreira <delsofelipe@gmail.com>
2023-10-28 23:51:42 -0300

    Create SimpleFighter

--- src/TestePersonagem/TestePersonagem.ts
+++
@@ -1,9 +0,0 @@
-import Character from '../Character';
-
-const personagem = new Character('tales');
-
-console.log(personagem.lifePoints);
-
-console.log(personagem.receiveDamage(10));
-
-console.log(personagem.lifePoints);
