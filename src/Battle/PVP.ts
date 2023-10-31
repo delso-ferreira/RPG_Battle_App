60fa3988c5c81b4e4ee489b89e8136eba2ab0209
delso-ferreira <delsofelipe@gmail.com>
2023-10-31 10:24:54 -0300

    Create PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,16 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {  
+  constructor(protected player1: Fighter, protected player2: Fighter) {
+    super(player1);
+  }   
+
+  fight(): number { // atacar até um dos dois ficar com menos 1 de vida        
+    while (this.player1.lifePoints !== -1 && this.player2.lifePoints !== -1) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);      
+    }
+    return super.fight(); // resultado
+  }
+}
